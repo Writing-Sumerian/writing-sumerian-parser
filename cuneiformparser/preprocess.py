26fbfa6264489c0a91dd7a55747ad4d84dae9414
Marc Endesfelder <marc.endesfelder@gmail.com>
2024-07-06 10:43:06 +0200

    Remove legacy code

--- cuneiformparser/preprocess.py
+++ cuneiformparser/preprocess.py
@@ -15,11 +15,3 @@ def preprocess(text, f):
             else:
                 res += f(part) if not level else part
     return res
-
-#def preprocess(text, f):
-#    parts = re.split(r'(?P<comment>\((?:[^\(\)]+|(?&comment))*\))|("[^"]*"|_[^_]*_)', text)
-#    res = ''
-#    for i, part in enumerate(parts):
-#        if part:
-#            res += f(part) if not i%3 else part
-#    return res
